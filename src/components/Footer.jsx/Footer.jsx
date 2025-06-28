4ad58bd4ad1639d9e241b2b9e9cd185171be8ee7
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-06-28 16:14:00 +0600

    create footer

--- src/components/Footer.jsx/Footer.jsx
+++ src/components/Footer.jsx/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="footer sm:footer-horizontal footer-center bg-primary text-secondary p-4">
+            <aside>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by Abu Hurayra Syam</p>
+            </aside>
+        </footer>
     );
 };
 
